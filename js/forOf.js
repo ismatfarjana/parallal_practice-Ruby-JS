45d45a4945faec60ff093bb5b30dd99dd971f426
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2021-02-28 09:30:35 +1100

    use of for..of and for..in loops over an array

---
+++ js/forOf.js
@@ -0,0 +1,16 @@
+const rainbow = [
+  "violate",
+  "indigo",
+  "blue",
+  "green",
+  "yellow",
+  "orange",
+  "red"
+];
+
+for (const color of rainbow) {
+  console.log(`One color of the rainbow is: ${color}`);
+}
+for (const color in rainbow) {
+  console.log(color);
+}
